7e3f4915c1f6378f6424bca4eddf694184bf4d2a
takafumishimura-ops <takafumi.shimura@vinc.co.jp>
2026-09-06 23:47:47 +0900

    Update next.config.js to disable page content caching
    Disable caching of page content to ensure latest data is fetched immediately after registration.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 画面遷移(クリックでのページ移動)時にNext.jsがページ内容を一時的に
+  // キャッシュしてしまい、登録直後のデータが一覧に反映されないことがあるため、
+  // このアプリでは常に最新のデータを取得するように無効化する。
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+    },
+  },
 };
 
 module.exports = nextConfig;
